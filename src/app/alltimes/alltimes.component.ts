f91d9f0dc5db109429fa097eb91bb4aaf4cb9cd3
nkunchan <nikita.999.d@gmail.com>
2017-09-29 23:20:35 -0400

    Mutation commit

--- src/app/alltimes/alltimes.component.ts
+++ src/app/alltimes/alltimes.component.ts
@@ -4,6 +4,7 @@ import Dexie from 'dexie';
 import { Observable } from "rxjs";
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 const MAX_EXAMPLE_RECORDS = 1000;
 
 @Component({
@@ -15,6 +16,8 @@ export class AlltimesComponent implements OnInit {
 
   @ViewChild("dt") dt : DataTable;
 
+  projectForm: FormGroup;
+  
  
   allTimesheetData = [];
   
@@ -23,17 +26,24 @@ export class AlltimesComponent implements OnInit {
   allProjects = this.allProjectNames.map((proj) => {
     return { label: proj, value: proj }
   });
-
+  displayEditDialog = false;
   selectedRows: Array<any>;
 
   contextMenu: MenuItem[];
 
   recordCount : number;
-
-  constructor(private apollo: Apollo) { }
+  
+  constructor(private apollo: Apollo, private fb:FormBuilder) { }
 
   ngOnInit() {
-   
+    this.projectForm=this.fb.group({
+      User: ['', [Validators.required]],
+      Project: ['', [Validators.required]],
+      Category: ['', [Validators.required]],
+      StartTime: ['', [Validators.required]],
+      EndTime: ['', [Validators.required]]
+
+    })
     const AllClientsQuery = gql`
         query allTimesheets {
           allTimesheets {
@@ -57,6 +67,40 @@ export class AlltimesComponent implements OnInit {
 
 });
   
+}
+
+onSubmit() {
+  const user = this.projectForm.value.User;
+  const project = this.projectForm.value.Project;
+  const category = this.projectForm.value.Category;
+  const startTime = this.projectForm.value.StartTime;
+  const endTime = this.projectForm.value.EndTime;
+
+  const createTimesheet = gql`
+    mutation createTimesheet ($user: String!, $project: String!, $category: String!, $startTime: Int!, $endTime: Int!, $date: DateTime!) {
+      createTimesheet(user: $user, project: $project, category: $category, startTime: $startTime, endTime: $endTime, date: $date ) {
+        id
+      }
+    }
+  `;
+
+  this.apollo.mutate({
+    mutation: createTimesheet,
+    variables: {
+      user: user,
+      project: project,
+      category: category,
+      startTime: startTime,
+      endTime: endTime,
+      date: new Date()
+    }
+  }).subscribe(({ data }) => {
+    console.log('got data', data);
+    
+  }, (error) => {
+    console.log('there was an error sending the query', error);
+  });
+  this.displayEditDialog = false;
 }
 
 onEditComplete(editInfo) { }
